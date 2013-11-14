85535bc7d324a336cf25ad73251779b2cfc9bf98
Will Farrington <wfarr@github.com>
2013-11-13 22:15:37 -0800

    Extract notifications commands

--- src/commands.c
+++ src/commands.c
@@ -6,58 +6,8 @@
 #include "helper.h"
 #include "commands.h"
 #include "commands/help.h"
+#include "commands/notifications.h"
 #include "commands/status.h"
-
-static int
-enable_notifications_for_obj(int sd, const char* obj, const char* rest)
-{
-  (void)rest;
-
-  host*    hst;
-  service* svc;
-  nez_find_host_or_service(obj, &hst, &svc);
-
-  if (svc) {
-    enable_service_notifications(svc);
-    nsock_printf_nul(sd, "NOTIFICATIONS ENABLED FOR SERVICE: %s/%s\n", svc->host_name, svc->display_name);
-    return 200;
-  }
-
-  if (hst) {
-    enable_host_notifications(hst);
-    nsock_printf_nul(sd, "NOTIFICATIONS ENABLED FOR HOST: %s\n", hst->display_name);
-    return 200;
-  }
-
-  nsock_printf_nul(sd, "NO HOST OR SERVICE FOUND FOR: %s", obj);
-  return 404;
-}
-
-static int
-disable_notifications_for_obj(int sd, const char* obj, const char* rest)
-{
-  (void)rest;
-
-  host*    hst;
-  service* svc;
-  nez_find_host_or_service(obj, &hst, &svc);
-
-  if (svc) {
-    disable_service_notifications(svc);
-    nsock_printf_nul(sd, "NOTIFICATIONS DISABLED FOR SERVICE: %s/%s\n", svc->host_name, svc->display_name);
-    return 200;
-  }
-
-  if (hst) {
-    disable_host_notifications(hst);
-    nsock_printf_nul(sd, "NOTIFICATIONS DISABLED FOR HOST: %s\n", hst->display_name);
-    return 200;
-  }
-
-  nsock_printf_nul(sd, "NO HOST OR SERVICE FOUND FOR: %s", obj);
-  return 404;
-}
-
 
 static int
 schedule_downtime_for_obj(int sd, const char* obj, unsigned long minutes, char* comment_data)
@@ -301,20 +251,6 @@ nez_cmd_frenchman(int sd, char* object, char* rest)
   (void)rest;
   nsock_printf_nul(sd, "yolochocinco!!!!!!\n");
   return 420;     // easter egg
-}
-
-static int
-nez_cmd_enable_notifications(int sd, char* object, char* rest)
-{
-  (void)rest;
-  return enable_notifications_for_obj(sd, object, rest);
-}
-
-static int
-nez_cmd_disable_notifications(int sd, char* object, char* rest)
-{
-  (void)rest;
-  return disable_notifications_for_obj(sd, object, rest);
 }
 
 static int
---
+++ src/commands/notifications.c
@@ -0,0 +1,69 @@
+#include <nagios/nagios.h>
+
+#include "../helpers.h"
+
+#include "notifications.h"
+
+static int
+enable_notifications_for_obj(int sd, const char* obj, const char* rest)
+{
+  (void)rest;
+
+  host*    hst;
+  service* svc;
+  nez_find_host_or_service(obj, &hst, &svc);
+
+  if (svc) {
+    enable_service_notifications(svc);
+    nsock_printf_nul(sd, "NOTIFICATIONS ENABLED FOR SERVICE: %s/%s\n", svc->host_name, svc->display_name);
+    return 200;
+  }
+
+  if (hst) {
+    enable_host_notifications(hst);
+    nsock_printf_nul(sd, "NOTIFICATIONS ENABLED FOR HOST: %s\n", hst->display_name);
+    return 200;
+  }
+
+  nsock_printf_nul(sd, "NO HOST OR SERVICE FOUND FOR: %s", obj);
+  return 404;
+}
+
+static int
+disable_notifications_for_obj(int sd, const char* obj, const char* rest)
+{
+  (void)rest;
+
+  host*    hst;
+  service* svc;
+  nez_find_host_or_service(obj, &hst, &svc);
+
+  if (svc) {
+    disable_service_notifications(svc);
+    nsock_printf_nul(sd, "NOTIFICATIONS DISABLED FOR SERVICE: %s/%s\n", svc->host_name, svc->display_name);
+    return 200;
+  }
+
+  if (hst) {
+    disable_host_notifications(hst);
+    nsock_printf_nul(sd, "NOTIFICATIONS DISABLED FOR HOST: %s\n", hst->display_name);
+    return 200;
+  }
+
+  nsock_printf_nul(sd, "NO HOST OR SERVICE FOUND FOR: %s", obj);
+  return 404;
+}
+
+int
+nez_cmd_enable_notifications(int sd, char* object, char* rest)
+{
+  (void)rest;
+  return enable_notifications_for_obj(sd, object, rest);
+}
+
+int
+nez_cmd_disable_notifications(int sd, char* object, char* rest)
+{
+  (void)rest;
+  return disable_notifications_for_obj(sd, object, rest);
+}
