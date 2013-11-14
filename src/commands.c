d5334ceafe2d3f3e4e0e56edd9ae8f9941b3cb2f
Will Farrington <wfarr@github.com>
2013-11-13 21:58:52 -0800

    Extract help command to own files

--- src/commands.c
+++ src/commands.c
@@ -5,27 +5,7 @@
 
 #include "helper.h"
 #include "commands.h"
-
-static int
-display_help(int sd)
-{
-  nsock_printf_nul(sd, "Query handler for actually doing useful shit with this socket.\n"
-       "Available commands:\n"
-       "  status <host|service>                          Display the status of a host or service\n"
-       "  check <host|service>                           Schedule a re-check of the host or service\n"
-       "\n"
-       "  enable_notifications <host|service>            Enable notifications for a host or host-service\n"
-       "  disable_notifications <host|service>           Disable notifications for a host or host-service\n"
-       "\n"
-       "  acknowledge <host|service> [<comment>]         Acknowledge a host/service problem (opt. comment)\n"
-       "  unacknowledge <host|service>                   Unacknowledge a host/service problem\n"
-       "\n"
-       "  downtime <host|service> [<minutes> <comment>]  Schedule downtime for a host/service (opt. num minutes, comment)\n"
-       "\n"
-       "  problems [<svcgroup|hstgroup> <state>]         Display all services in a non-OK state\n"
-       );
-  return 200;
-}
+#include "commands/help.h"
 
 static void
 find_host_or_service(const char* name, host** hst, service** svc)
@@ -403,14 +383,6 @@ force_nagios_check(int sd, char* obj)
 }
 
 // COMMANDS
-
-static int
-nez_cmd_help(int sd, char* object, char* rest)
-{
-  (void)object;
-  (void)rest;
-  return display_help(sd);
-}
 
 static int
 nez_cmd_frenchman(int sd, char* object, char* rest)
---
+++ src/commands/help.h
@@ -0,0 +1,6 @@
+#ifndef NEZ_COMMANDS_HELP_H
+#define NEZ_COMMANDS_HELP_H
+
+int nez_cmd_help(int sd, char* object, char* rest);
+
+#endif
